2cdc33adfe8a44d79943038fa5cd3b65aa3df23d
Wellington Galvão <wellingtongalvao96@gmail.com>
2024-03-18 20:48:54 -0300

    Configuração docker

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log("Server is running");
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", async () => {
+  return { message: "Hello, world!" };
+});
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server is running on port 3333");
+});
